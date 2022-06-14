65da048977b4b3fd9db54ed1d020ae0ada3bbd0a
“Vidya <vidya.ummadireddy@xandr.com>
2022-06-14 15:41:20 +0530

    counter app changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import navbar from './components/navbar';
+import Counters from './components/counters';
+import React, { Component } from 'react';
+import NavBar from './components/navbar';
+
+class App extends Component{
+  state = { 
+    counters:[
+        {id:1 ,value:4},
+        {id:2 ,value:0},
+        {id:3 ,value:2},
+        {id:4 ,value:0},
+    ]
+ } ;
+ handleDelete=(counterId)=>{
+     const newCounters = this.state.counters.filter(c=> c.id!==counterId);
+     this.setState({counters:newCounters});
+ };
+ handleReset=()=>{
+     const counters=this.state.counters.map(c => {
+         c.value=0;
+         return c;
+     })
+     this.setState({counters});
+ };
+ handleIncrement=counter=>{
+     const counters=[...this.state.counters];
+     const index=counters.indexOf(counter);
+     counters[index] ={...counter};
+     counters[index].value++;
+     this.setState({counters});
+ };
+
+  render(){
+    return(
+      <React.Fragment>
+        <NavBar totalCounters={this.state.counters.filter(c =>c.value>0).length}/>
+        <main className='container'>
+          <Counters onDelete={this.handleDelete} onIncrement={this.handleIncrement} onReset={this.handleReset} counters={this.state.counters}/>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
